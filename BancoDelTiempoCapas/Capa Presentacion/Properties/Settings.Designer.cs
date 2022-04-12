0aa47e2906c4b9b7b70966b4a2f2ded341c610eb
mamaroa <mamaroa@uoc.edu>
2022-04-12 12:55:19 -0700

    categoriaDeServicio

--- BancoDelTiempoCapas/Capa Presentacion/Properties/Settings.Designer.cs
+++ BancoDelTiempoCapas/Capa Presentacion/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CapaPresentacion.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,17 @@ namespace CapaPresentacion.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-UJ0VFTKS\\SQLSERVER;Initial Catalog=BancoDelTiempo;Integrated S" +
+            "ecurity=True")]
+        public string BancoDelTiempoConnectionString {
+            get {
+                return ((string)(this["BancoDelTiempoConnectionString"]));
             }
         }
     }
