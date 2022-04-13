a552735cd31421b75ab5ae672ca0f8bbd71184c3
jpodeschva <92250265+jpodeschva@users.noreply.github.com>
2022-04-13 11:15:44 +0200

    modificar carpetas

--- BancoDelTiempoCapas/CapaPresentacion/Program.cs
+++ BancoDelTiempoCapas/CapaPresentacion/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Capa_Presentacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Capa_Presentacion
+namespace CapaPresentacion
 {
     static class Program
     {
