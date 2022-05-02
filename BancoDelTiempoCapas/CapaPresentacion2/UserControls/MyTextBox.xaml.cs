f6d0484b7ed87f34ec1783426f81f89676811d1b
laurazp <lzafrap@uoc.edu>
2022-05-02 15:23:13 +0100

    Creados controles de TextBox

---
+++ BancoDelTiempoCapas/CapaPresentacion2/UserControls/MyTextBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CapaPresentacion2.UserControls
+{
+    public partial class MyTextBox : UserControl
+    {
+        public MyTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public String Hint
+        {
+            get { return (String)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty = DependencyProperty.Register
+            ("Hint", typeof(String), typeof(MyTextBox));
+
+    }
+}
